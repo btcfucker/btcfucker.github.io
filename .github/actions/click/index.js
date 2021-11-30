cb502c374423ff7e544e5ea28f90f9d5a6db0a9b
Farbod <cppox1@gmail.com>
2021-11-30 14:55:42 +0400

    .

--- .github/actions/click/index.js
+++ .github/actions/click/index.js
@@ -1,14 +1,16 @@
 const { chromium } = require('playwright');
 const launchOptions = {
     proxy: {
-        server: '45.127.246.98:8080'
+        server: '178.212.54.137:8080'
     }
 };
 (async () => {
+    try{
   const browser = await chromium.launch(launchOptions);
   const page = await browser.newPage();
   await page.goto('https://click.a-ads.com/1845539/98644/');
   await page.waitForTimeout(10000);
   await page.screenshot({ path: `example.png` });
-  await browser.close();
+  await browser.close();}
+  catch{}
 })();

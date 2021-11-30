be76185567674a5950b9e2e6e3366aae8297f05f
Farbod <cppox1@gmail.com>
2021-11-30 14:40:11 +0400

    .

--- .github/actions/click/index.js
+++ .github/actions/click/index.js
@@ -1,11 +1,10 @@
-const playwright = require('playwright');
+const { chromium } = require('playwright');
+
 (async () => {
-    const browser = await playwright.chromium.launch({ args: ['--no-sandbox'] });
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('https://click.a-ads.com/1845539/98644/');
-    await page.waitForTimeout(10000);
-    await page.screenshot({ path: `example.png` });
-    await browser.close();
-  
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  await page.goto('https://click.a-ads.com/1845539/98644/');
+  await page.waitForTimeout(10000);
+  await page.screenshot({ path: `example.png` });
+  await browser.close();
 })();

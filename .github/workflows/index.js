7d82834102154f6fe3a1a17cb1aae2ba146c580e
Farbod <cppox1@gmail.com>
2021-11-30 12:36:28 +0400

    .

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -4,8 +4,8 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://google.com');
+  await page.goto('https://click.a-ads.com/1845539/98644/');
   await page.screenshot({ path: 'example.png' });
-
+  await page.waitForTimeout(10000);
   await browser.close();
 })();
